e2033f727a83674fb526cb79b9f0bed79bb767a1
Anurup Reddy <koduruanurupreddy2@gmail.com>
2023-02-07 23:28:28 +0530

    initialized firebase project

---
+++ src/misc/firebase.jsx
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDwaR9Bm_t97AxA6Sc-DNelCcucKeyVVeY',
+  authDomain: 'chat-web-app-227b4.firebaseapp.com',
+  projectId: 'chat-web-app-227b4',
+  storageBucket: 'chat-web-app-227b4.appspot.com',
+  messagingSenderId: '1079508504802',
+  appId: '1:1079508504802:web:a3b37033efe58af5560ebb',
+};
+
+const app = firebase.initializeApp(config);
