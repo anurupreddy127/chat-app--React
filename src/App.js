74ea658edc3a7ee2ca3ea647bfbcafe8254ad739
Anurup Reddy <koduruanurupreddy2@gmail.com>
2023-02-08 16:25:56 +0530

    added route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import 'rsuite/dist/rsuite.min.css';
 import './styles/main.scss';
+import { Switch } from 'react-router';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import PublicRoute from './components/PublicRoute';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <SignIn />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
