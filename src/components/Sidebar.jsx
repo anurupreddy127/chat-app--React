ce206bf7f4a154a47b30b7a5d35e9efe400ba24f
Anurup Reddy <koduruanurupreddy2@gmail.com>
2023-02-13 15:15:19 +0530

    started building sidebar components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import DashboardToogle from './dashboard/DashboardToogle';
+
+const Sidebar = () => {
+  return (
+    <div className="h-100 pt-2">
+      <div>
+        <DashboardToogle />
+      </div>
+      bottom
+    </div>
+  );
+};
+
+export default Sidebar;
