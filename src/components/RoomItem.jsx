9dec647bc6d996c27181b78dfeba03178690e51a
Anurup Reddy <koduruanurupreddy2@gmail.com>
2023-02-25 21:48:29 +0530

    created chat rooms

--- src/components/RoomItem.jsx
+++ src/components/RoomItem.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import TimeAgo from 'timeago-react';
 
-const RoomItem = () => {
+const RoomItem = ({ room }) => {
+  const { createdAt, name } = room;
+
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
-        <h3 className="text-disapper">Room Name</h3>
-        <TimeAgo datetime={new Date()} className="font-formal text-black-45" />
+        <h3 className="text-disapper">{name}</h3>
+        <TimeAgo
+          datetime={new Date(createdAt)}
+          className="font-formal text-black-45"
+        />
       </div>
 
       <div className="d-flex align-items-center text-black-70">
